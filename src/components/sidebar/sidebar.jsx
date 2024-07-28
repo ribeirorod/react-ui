722a452898eb4969fd62ac4675ac0cb46945cc5b
rodolfo.ribeiro <50660568+ribeirorod@users.noreply.github.com>
2024-07-28 21:54:58 +0200

    Delete src directory

--- src/components/sidebar/sidebar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import BaseSidebar from './base-sidebar';
-
-const SideBar = ({
-  children, ...props}) => {
-  return <BaseSidebar {...props}>{children}</BaseSidebar>;
-};
-
-export default SideBar;
